a3301cc8d57763abb205395beb0c898b7ee11df2
Sebastien Boiteau <n9537058@qut.edu.au>
2020-09-18 14:33:55 +1000

    Update all_readings_try.py

--- air_sensor/all_readings_try.py
+++ air_sensor/all_readings_try.py
@@ -17,9 +17,12 @@ ltr559=LTR559()
 
 def get_noise():
     amps = noise.get_amplitudes_at_frequency_ranges([
-        (100, 200),
-        (500, 600),
-        (1000, 1200)
+        (20, 200), #100 200
+        (200, 600), #500 600
+        (600, 1000), # 100 1200
+        (1000,1500),
+        (1500,2000)
+
     ])
     amps = [n * 32 for n in amps]
     return amps
@@ -36,31 +39,71 @@ def get_temperature():
     temperature = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
     return temperature
 
+def get_gases():
+    readings=gas.read_all()
+    oxidised=round(readings.oxidising, 2)
+    reduced=round(gas.read_reducing(),2)
+    nh3=round(readings.nh3,2)
+    gases=[oxidised,reduced,nh3]
+    return gases
+
+start_time = time.time()
+
+gases_threshold=[0,0,0]
+
 try:
     while True:
 
-        readings=gas.read_all()
-        oxidised=readings.oxidising
-	print("oxidised=", oxidised )
-        reduced=gas.read_reducing()
-	print("reduced=", reduced)
-        nh3=readings.nh3
-	print("nh3=",nh3)
+        elapsed_time = time.time() - start_time
         
-        pressure=bme280.get_pressure()
-	print("pressure=", pressure)
-        humidity=bme280.get_humidity()
-	print("humidity=",humidity)
-        light=ltr559.get_lux()
-	print("light=",light)
-        
-        temperature= get_temperature()
-	print("temperature=",temperature)
+        if elapsed_time<120:
+            print("The sensors are calibrating, please wait two minutes.")
+            gases_calibration=get_gases()
+            pressure_calibration=bme280.get_pressure()
+	        humidity_calibration=bme280.get_humidity()
+	        light_calibration=ltr559.get_lux()
+	        temperature_calibration= get_temperature()
+	        noise_lvl_calibration=get_noise()
 
-        noise_lvl=get_noise()
-	print("The noise levels are:", noise_lvl)
-	
-	time.sleep(5)
+        elif elapsed_time==120:
+            gases_threshold=get_gases()
 
+        elif elapsed_time>120:
+	        pressure=bme280.get_pressure()
+	        print("pressure=", pressure)
+            humidity=bme280.get_humidity()
+	        print("humidity=",humidity)
+            light=ltr559.get_lux()
+	        print("light=",light)
+            temperature= get_temperature()
+	        print("temperature=",temperature)
+            noise_lvl=get_noise()
+	        print("The noise levels are:", noise_lvl)
+
+            gases=get_gases()
+
+            if gases[0]>gases_threshold[0]:
+                print("OX current value: ",gases[0]," OX threshold: ",gases_threshold[0], " OX concentration is increasing")
+            elif gases[0]<gases_threshold[0]:
+                print("OX current value: ",gases[0]," OX threshold: ",gases_threshold[0], " OX concentration is reducing)
+            else:
+                print("OX current value: ",gases[0]," OX threshold: ",gases_threshold[0], " OX concentration is the same as threshold)
+
+            if gases[1]>gases_threshold[1]:
+                print("RED current value: ",gases[1]," RED threshold: ",gases_threshold[1], " RED concentration is reducing")
+            elif gases[1]<gases_threshold[1]:
+                print("RED current value: ",gases[1]," RED threshold: ",gases_threshold[1], " RED concentration is increasing")
+            else:
+                print("RED current value: ",gases[1]," RED threshold: ",gases_threshold[1], " RED concentration is the same as threshold)
+
+            if gases[2]>gases_threshold[2]:
+                print("NH3 current value: ",gases[2]," NH3 threshold: ",gases_threshold[2], " NH3 concentration is reducing")
+            elif gases[2]<gases_threshold[2]:
+                print("NH3 current value: ",gases[2]," NH3 threshold: ",gases_threshold[2], " NH3 concentration is increasing")
+            else:
+                print("NH3 current value: ",gases[2]," NH3 threshold: ",gases_threshold[2], " NH3 concentration is the same)
+
+            time.sleep(2)
+            
 except KeyboardInterrupt:
     pass
